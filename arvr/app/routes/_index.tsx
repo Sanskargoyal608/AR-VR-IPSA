aa8095f046c5ea606c666f28b421e68e3b0106b9
Sanskargoyal608 <sanskargoyal608@gmail.com>
2024-08-12 00:57:32 +0530

    set up completed

---
+++ arvr/app/routes/_index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '~/components/Layout';
+
+export default function Index() {
+  return (
+    <Layout>
+      <h1>Welcome to My Website</h1>
+      <p>This is the homepage content.</p>
+    </Layout>
+  );
+}
